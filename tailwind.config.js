7a5d85470dcc84d667784e0954f5b4c96a80c917
MD Jobayer Adnan <you@example.com>
2024-12-11 19:05:38 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
-
